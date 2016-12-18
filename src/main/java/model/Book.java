b6561477d00b988f797f8d80e66dc11341ee637c
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2016-12-18 11:54:19 -0500

    Initial Commit for JPA project

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,82 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+/**
+ * The persistent class for the book database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Book.findAll", query="SELECT b FROM Book b")
+public class Book implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private String id;
+
+	private String description;
+
+	private String isbn;
+
+	private int nbOfPage;
+
+	private String title;
+
+	private BigDecimal unitCost;
+
+	public Book() {
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getIsbn() {
+		return this.isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getNbOfPage() {
+		return this.nbOfPage;
+	}
+
+	public void setNbOfPage(int nbOfPage) {
+		this.nbOfPage = nbOfPage;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public BigDecimal getUnitCost() {
+		return this.unitCost;
+	}
+
+	public void setUnitCost(BigDecimal unitCost) {
+		this.unitCost = unitCost;
+	}
+
+}
